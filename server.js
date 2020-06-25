dfceb6b023615771d6e70a924fbf670b6f36501d
mr-gateau <tagot.nicolas@gmail.com>
2020-06-25 08:57:11 +0200

    ajout de la route template

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-const connection = require('./conf.js');
+const port = 3004;
+
 const router = express.Router();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
 
 const aboutRoute = require('./routes/about');
-
+const templRoute = require('./routes/template');
 app.use('/about', aboutRoute);
+app.use('/templates', templRoute);
 
 
 

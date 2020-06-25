cef2b135db6158be52291187661db58cd649cd27
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-06-25 22:09:53 +0200

    commit before pull

--- Routes/response.js
+++ Routes/response.js
@@ -19,6 +19,7 @@ router.post('/', (req, res) => {
 });
 
 router.get('/', (req, res) => {
+    const id = req.params.id;
     connection.query('SELECT * FROM response', 
     (err, results) => {
         if(err) {
@@ -28,6 +29,36 @@ router.get('/', (req, res) => {
             });
         }
         res.json(results)
+    });
+});
+
+//GET ALL RESPONSE WHERE medic = 1
+router.get('/medic', (req, res) => {
+    connection.query('SELECT * FROM response WHERE medic = 1', (error, results) => {
+        if(error) {
+            return res.status(500).json({ error: err.message, sql: err.sql });
+        }
+        return res.status(200).json(results);
+    });
+});
+
+// GET ALL response WHERE perso = 1
+router.get('/perso', (req, res) => {
+    connection.query('SELECT * FROM response WHERE perso = 1', (error, results) => {
+        if(error) {
+            return res.status(500).json({ error: err.message, sql: err.sql });
+        }
+        return res.status(200).json(results);
+    });
+});
+
+// GET ALL response WHERE sante = 1
+router.get('/sante', (req, res) => {
+    connection.query('SELECT * FROM response WHERE sante = 1', (error, results) => {
+        if(error) {
+            return res.status(500).json({ error: err.message, sql: err.sql });
+        }
+        return res.status(200).json(results);
     });
 });
 
--- conf.js
+++ conf.js
@@ -3,8 +3,8 @@ const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 port : '3306',
 user :  'root', // le nom d'utilisateur
-password :  'ploufeur', // le mot de passe
-database :  'hacka' // le nom de la base de données
+password :  'jecode4wcs', // le mot de passe
+database :  'bdd_hackathon' // le nom de la base de données
 });
 module.exports = connection;
 // NOTE:
